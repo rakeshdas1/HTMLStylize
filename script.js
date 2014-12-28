8b24a065f749ec82ffd2f5a5a493e9f064373f7b
Rakesh Das <rakeshdaskowshik@gmail.com>
2014-12-27 23:49:17 -0600

    Jquery stuff

---
+++ script.js
@@ -0,0 +1,11 @@
+$document.ready(function (){
+    $("button").click(function() {
+        var word = $("#wordToDefine").val;
+        var xml = "http://www.dictionaryapi.com/api/v1/references/collegiate/xml/" + word + "?key=0cec1fd4-d9d1-4ccc-9ad2-f996151173eb",
+            xmlDoc = $.parseXML( xml ),
+            $xml = $( xmlDoc ),
+            $definition = $xml.find("ew");
+        $("#definition").text($definition.text());
+        alert($definition.text());
+    });
+});
