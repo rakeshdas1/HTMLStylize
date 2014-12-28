e5e4176dfee7602317cb2390c03441675a63175c
Rakesh Das <rakeshdaskowshik@gmail.com>
2014-12-28 12:51:53 -0600

    jQuery

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-$document.ready(function (){
+$(document).ready(function (){
+    $("html").fadeIn(1500);
     $("button").click(function() {
-        var word = $("#wordToDefine").val;
-        var xml = "http://www.dictionaryapi.com/api/v1/references/collegiate/xml/" + word + "?key=0cec1fd4-d9d1-4ccc-9ad2-f996151173eb",
+        var word = $("#wordToDefine").val();
+        /*var xml = "http://www.dictionaryapi.com/api/v1/references/collegiate/xml/" + word + "?key=0cec1fd4-d9d1-4ccc-9ad2-f996151173eb",
             xmlDoc = $.parseXML( xml ),
             $xml = $( xmlDoc ),
-            $definition = $xml.find("ew");
-        $("#definition").text($definition.text());
-        alert($definition.text());
+            $definition = $xml.find("ew");*/
+        $("#definition").text(word);
     });
 });
+
+
