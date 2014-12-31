b5f64358fc7959ee90b5698987dff8a275dbdd80
Rakesh Das <rakeshdaskowshik@gmail.com>
2014-12-30 23:39:13 -0600

    Messing with rss

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 $(document).ready(function (){
-    $("html").fadeIn(10000);
-    $("button").click(function() {
-        var word = $("#wordToDefine").val();
-        /*var xml = "http://www.dictionaryapi.com/api/v1/references/collegiate/xml/" + word + "?key=0cec1fd4-d9d1-4ccc-9ad2-f996151173eb",
-            xmlDoc = $.parseXML( xml ),
-            $xml = $( xmlDoc ),
-            $definition = $xml.find("ew");*/
-        $("#definition").text(word);
+    $("html").fadeIn(2000);
+    $.get("test.xml", {}, function(xml){
+        $('testNumber', xml).each(function (i) {
+            //console.log(i);
+            $("#content").append("<p>" + i + "</p>");
+        })
+    });
+    /*$.ajax({
+        type:"GET",
+        url:"books.xml",
+        dataType:"xml",
+        success: function(xml) {
+             $.parseXML(xml).find('book').each(function(index){
+                 console.log(index);
+             });
+        }
+    });*/
+    $.ajax("test", function(rss){
+        xml = $.parseXML(rss);
+        $(xml).find("testNumber").each(function(){
+            console.log(this);
+        });
     });
 });
 
